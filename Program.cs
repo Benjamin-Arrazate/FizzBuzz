fc73ca45a62ae6beb16debca559312a32adaed3c
Benjamin Arrazate <benjamin.arrazate@icloud.com>
2021-02-10 19:09:14 -0700

    Mi primer programa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = new int[100];
+
+            int x = 0;
+
+            for(int i = 0;i<100;i++)
+            {
+                x = i + 1;
+
+                numeros[i] = x;
+                
+            }
+            
+            for(int i = 0;i<numeros.Length;i++)
+            {
+
+                int mod3 = (numeros[i] % 3);
+                int mod5 = (numeros[i] % 5);
+
+                if(mod3==0 && mod5==0)
+                {  
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(mod3==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(mod5==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(numeros[i]);
+                }
+            }
         }
     }
 }
